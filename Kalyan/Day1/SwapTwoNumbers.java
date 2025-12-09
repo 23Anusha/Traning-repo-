59be1cfa201bcb8f66f1c6c33b1436fcf327f256
KALYAN G V <152889362+kalyangvk03@users.noreply.github.com>
2025-12-09 21:09:52 +0530

    Update SwapTwoNumbers.java
    Added a method to swap two numbers without a temporary variable using arithmetic operations.

--- Kalyan/Day1/SwapTwoNumbers.java
+++ Kalyan/Day1/SwapTwoNumbers.java
@@ -9,12 +9,17 @@ public class SwapTwoNumbers {
 	value1=value2;
 	value2=temp;
 	
-
+	//Without Temporary Variable and Using Operators
+	int a = 10;
+	int b = 20;
 	
+	a = a + b; 
+	b = a - b; 
+	a = a - b; 
+
+	System.out.println("a = " + a + ", b = " + b);	
 	System.out.println("The swapped numbers are Value 1 is " +value1 + " and Value 2 is "  + value2);
 	
-	
-
 	}
 
 }
