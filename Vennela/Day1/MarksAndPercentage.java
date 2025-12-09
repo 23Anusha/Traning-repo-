912bfe64148def76b525970d60f229294f6739c9
Vennela2795 <vennelas782@gmail.com>
2025-12-09 22:35:17 +0530

    Added Vennela Day1 files

---
+++ Vennela/Day1/MarksAndPercentage.java
@@ -0,0 +1,18 @@
+package Practice;
+
+public class MarksAndPercentage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int Kannada = 108;
+		int English = 76;
+		int Hindi = 88;
+		int Mathematics = 63;
+		int Science = 67;
+		int Socialscience = 82;
+		int Total = Kannada+English+Hindi+Mathematics+Science+Socialscience;
+		float Percentage = Total/6f;
+		System.out.println("The Total Marks Are " + Total + "Out of 600 And The Percentage is " + Percentage);
+	}
+
+}
